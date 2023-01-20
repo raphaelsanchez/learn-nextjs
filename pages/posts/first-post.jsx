3ac18898035f89eb69a43d70933455e6755a3e6e
Raphael Sanchez <hello@raphaelsanchez.design>
2023-01-20 23:12:46 +0100

    update posts

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,9 +1,23 @@
 import Link from 'next/link'
+import Head from 'next/head'
+import Script from 'next/script'
+import Layout from '../../components/Layout'
+
 export default function FirstPost() {
   return (
-    <>
-      <h1>First Post</h1>
-      <Link href="/">Back to home</Link>
-    </>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://unpkg.com/feather-icons"
+        strategy="afterInteractive"
+        onLoad={() => feather.replace()}
+      />
+      <h1>
+        First Post <i data-feather="circle"></i>
+      </h1>
+      <Link href="/">← Back to home</Link>
+    </Layout>
   )
 }
--- pages/posts/second-post.jsx
+++ pages/posts/second-post.jsx
@@ -1,10 +1,18 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Layout from '../../components/Layout'
+
 
 export default function SecondPost() {
   return (
-    <>
-      <h1>Second Post</h1>
-      <Link href="/">Back to home</Link>
-    </>
+    <Layout>
+      <Head>
+        <title>Second Post</title>
+      </Head>
+      <h1>
+        Second Post <i data-feather="circle"></i>
+      </h1>
+      <Link href="/">← Back to home</Link>
+    </Layout>
   )
 }
